8fd08c3ef1bab7a969ddcaeead19579f7f34adbb
IsraelBuskila <israelad3@gmail.com>
2021-03-02 16:12:34 +0200

    change member to component

--- Client/src/Components/Subscriptions.js
+++ Client/src/Components/Subscriptions.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import Member from "../Views/Member";
+import Member from "./Member";
 
 const Subscriptions = () => {
   const [toggle, setToggle] = useState("All Members");
