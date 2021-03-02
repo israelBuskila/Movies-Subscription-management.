e635a0b53834b63d765ef443e2064fae753430cd
IsraelBuskila <israelad3@gmail.com>
2021-03-02 16:13:13 +0200

    add router

--- server/routes/membersAndSubsRoutes.js
+++ server/routes/membersAndSubsRoutes.js
@@ -5,4 +5,8 @@ const membersBL = require("../BL/membersBL");
 router.get("/", async (req, res, next) => {
   res.send(await membersBL.getAllSubscriptionsWithMembers());
 });
+
+router.post("/subMovies", async (req, res, next) => {
+  res.send(await membersBL.subMovies(req.body));
+});
 module.exports = router;
