5119a664098596bf4c1b4c2f9e7d2e7da81a7439
IsraelBuskila <israelad3@gmail.com>
2021-03-04 22:39:19 +0200

    add subscription to movie

--- Client/src/Components/AddSubs.js
+++ Client/src/Components/AddSubs.js
@@ -1,4 +1,3 @@
-import Dropdown from "react-dropdown";
 import "react-dropdown/style.css";
 import { useState, useEffect } from "react";
 import axios from "axios";
@@ -17,20 +16,50 @@ const AddSubs = (props) => {
       .then((resp) => setMovies(resp.data));
   }, []);
 
-  const subscribe = () => {};
+  const options = () => {
+    if (movies) {
+      return movies.map((option, index) => {
+        return (
+          <option value={option._id} key={index}>
+            {option.name}
+          </option>
+        );
+      });
+    }
+  };
+
+  const subscribe = async () => {
+    // if (choice == "0") {
+    //   setChoice(movies[0]._id);
+    //   console.log(choice);
+    // }
+
+    let obj = {
+      MemberId: props.member.MemberId,
+      Movie: { movieId: choice, date: date },
+    };
+    console.log(obj);
+    let resp = await axios.post(
+      "http://localhost:3001/subscriptions/addSub",
+      obj
+    );
+  };
 
   return (
     <div>
       <h4>Add a new movie</h4>
-      <Dropdown
-        options={movies}
-        placeholder="Select an option"
-        onChange={(movies) => setChoice(movies)}
-      />
+
+      <select onChange={(e) => setChoice(e.target.value)}>
+        <option value="" disabled selected>
+          Select Movie
+        </option>
+        {options()}
+      </select>
+
       <input type="text" onChange={(e) => setDate(e.target.value)} />
       <br />
-      <input type="button" value="Subscribe" onClick={subscribe()} />
-      <h1>{choice}</h1>
+      <input type="button" value="Subscribe" onClick={() => subscribe()} />
+      {choice}
     </div>
   );
 };
