447fe7b1b187d2c1f9ca99cb24d5a84343fe9524
IsraelBuskila <israelad3@gmail.com>
2021-03-07 20:57:06 +0200

    add the option to delete MEMBER

--- Client/src/Components/Member.js
+++ Client/src/Components/Member.js
@@ -1,11 +1,22 @@
 import { Link } from "react-router-dom";
 import AddSubs from "./AddSubs";
 import { useState } from "react";
+import axios from "axios";
 
 const Member = (props) => {
   const [toggle, setToggle] = useState(false);
-  const edit = () => {};
-  const deleteMember = () => {};
+
+  //button to edit member -
+  const edit = () => {
+    props.call(props.member);
+  };
+
+  const deleteMember = () => {
+    axios.get(
+      "http://localhost:3001/subscriptions/deleteMember/" +
+        props.member.MemberId
+    );
+  };
   const moviesWathced = () => {
     return props.member.MoviesWathced.map((item, index) => {
       return (
@@ -29,8 +40,8 @@ const Member = (props) => {
       <br />
       City: {props.member.City}
       <br />
-      <input type="button" value="Edit" onClick={edit()} />
-      <input type="button" value="Delete" onClick={deleteMember()} />
+      <input type="button" value="Edit" onClick={edit} />
+      <input type="button" value="Delete" onClick={deleteMember} />
       <h3>Movies Watched</h3>
       <input
         type="button"
