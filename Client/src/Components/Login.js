9191c3b89b4e253ec1828faaf0f2968daab7d20b
IsraelBuskila <israelad3@gmail.com>
2021-03-18 17:31:25 +0200

    add material Ui

--- Client/src/Components/Login.js
+++ Client/src/Components/Login.js
@@ -1,72 +1,39 @@
-import React, { useState, useEffect } from "react";
-
+import React, { useState } from "react";
+import {
+  Button,
+  TextField,
+  Typography,
+  Container,
+  CssBaseline,
+  Avatar,
+  Grid,
+} from "@material-ui/core";
 import { useHistory, Link } from "react-router-dom";
 import axios from "axios";
+import { useStyles } from "../style/style";
+//import {LockOutlinedIcon} from "@material-ui/icons/LockOutlinedIcon";
 
 function Login() {
+  const classes = useStyles();
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
 
   let history = useHistory();
 
-  // const submit = () => {
-  //   console.log(userName);
-  //   let obj = {
-  //     UserName: userName,
-  //     Password: password,
-  //   };
-  //   axios.post("http://localhost:3001/", obj).then(
-  //     (response) => {
-  //       if (response.data == true) {
-  //         axios
-  //           .post("http://localhost:3001/users/getPermissions", {
-  //             userName: userName,
-  //           })
-  //           .then((resp) => {
-  //             console.log(resp.data.Permissions);
-  //             setPermissions(resp.data.Permissions);
-  //             setFirstName(resp.data.FirstName);
-  //           });
-  //         console.log(permissions);
-  //         console.log(firstName);
-  //         let userInfo = {
-  //           UserName: obj.UserName,
-  //           FirstName: firstName,
-  //           Login: true,
-  //           Permissions: permissions,
-  //         };
-  //         sessionStorage.setItem("userInfo", JSON.stringify(userInfo));
-  //         return history.push("/main");
-  //       } else if (response.data === "User does not exist !") {
-  //         alert(response.data);
-  //         return history.push("/");
-  //       } else {
-  //         alert("Incorrect password !");
-  //         return history.push("/");
-  //       }
-  //     },
-  //     (error) => {
-  //       console.log(error);
-  //     }
-  //   );
-  // };
-
   const submit = async () => {
-    console.log(userName);
     let obj = {
       UserName: userName,
       Password: password,
     };
     let response = await axios.post("http://localhost:3001/", obj);
-    console.log(response);
-    if (response.data == true) {
+
+    if (response.data === true) {
       let resp = await axios.post(
         "http://localhost:3001/users/getPermissions",
         {
           userName: userName,
         }
       );
-      console.log(resp.data.Permissions);
 
       let userInfo = {
         UserName: obj.UserName,
@@ -86,20 +53,58 @@ function Login() {
   };
 
   return (
-    <div>
-      <h1>Log in Page</h1>
-      <br />
-      User Name:
-      <input type="text" onChange={(e) => setUserName(e.target.value)} />
-      <br />
-      Password:
-      <input type="text" onChange={(e) => setPassword(e.target.value)} />
-      <br />
-      <input type="button" onClick={submit} value="Login" />
-      <br />
-      New User?: <Link to={"/createAccount"}> Create Account</Link>
-      <br />
-    </div>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>{/* <LockOutlinedIcon /> */}</Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <TextField
+          type="text"
+          name="username"
+          onChange={(e) => setUserName(e.target.value)}
+          label="Enter your username"
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          autoFocus
+          autoComplete="username"
+        />
+        <TextField
+          type="password"
+          name="password"
+          onChange={(e) => setPassword(e.target.value)}
+          label="Enter your password"
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          autoComplete="current-password"
+        />
+        <Button
+          className={classes.submit}
+          type="button"
+          value="login"
+          onClick={submit}
+          color="primary"
+          variant="contained"
+          fullWidth
+        >
+          Login
+        </Button>
+        <Grid item>
+          <Link to={"/createAccount"} variant="body2">
+            {"Don't have an account? Sign Up"}
+          </Link>
+        </Grid>
+      </div>
+      {/* <Typography component="h5" variant="h5">
+      {err}
+    </Typography> */}
+    </Container>
   );
 }
 
